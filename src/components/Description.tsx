2fef2c1063b326d0e6e1f98367db6829a4920971
TuanAnh01032001 <freetowin999@gamil.com>
2025-05-28 16:43:19 +0700

    Chỉnh sửa  giao diện

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import BounceCards from './BounceCards';
+// import BounceCards from './BounceCards';
 import Discover from './Discover'; 
 
 // Import images
-import img1 from '../public/img/img1.jpg'
-import img2 from '../public/img/img2.jpg'
-import img3 from '../public/img/img3.jpg'
-import img4 from '../public/img/img4.jpg'
-import img5 from '../public/img/img5.jpg'
+// import img1 from '../public/img/img1.jpg'
+// import img2 from '../public/img/img2.jpg'
+// import img3 from '../public/img/img3.jpg'
+// import img4 from '../public/img/img4.jpg'
+// import img5 from '../public/img/img5.jpg'
 
 const Description = () => {
   // Actual property measurements and features
@@ -37,21 +37,21 @@ const Description = () => {
     return count;
   }
 
-  const images = [
-    img1,
-    img2,
-    img3,
-    img4,
-    img5
-  ];
+  // const images = [
+  //   img1,
+  //   img2,
+  //   img3,
+  //   img4,
+  //   img5
+  // ];
   
-  const transformStyles = [
-    "rotate(5deg) translate(-150px)",
-    "rotate(0deg) translate(-70px)",
-    "rotate(-5deg)",
-    "rotate(5deg) translate(70px)",
-    "rotate(-5deg) translate(150px)"
-  ];
+  // const transformStyles = [
+  //   "rotate(5deg) translate(-150px)",
+  //   "rotate(0deg) translate(-70px)",
+  //   "rotate(-5deg)",
+  //   "rotate(5deg) translate(70px)",
+  //   "rotate(-5deg) translate(150px)"
+  // ];
 
   return (
     
--- src/components/Discover.tsx
+++ src/components/Discover.tsx
@@ -100,21 +100,21 @@ const places = [
   }
 ];
 
-const cardVariants = {
-  hidden: { 
-    opacity: 0,
-    y: 20
-  },
-  visible: (i: number) => ({
-    opacity: 1,
-    y: 0,
-    transition: {
-      delay: i * 0.2,
-      duration: 0.5,
-      ease: "easeOut"
-    }
-  })
-};
+// const cardVariants = {
+//   hidden: { 
+//     opacity: 0,
+//     y: 20
+//   },
+//   visible: (i: number) => ({
+//     opacity: 1,
+//     y: 0,
+//     transition: {
+//       delay: i * 0.2,
+//       duration: 0.5,
+//       ease: "easeOut"
+//     }
+//   })
+// };
 
 const modalVariants = {
   hidden: { 
